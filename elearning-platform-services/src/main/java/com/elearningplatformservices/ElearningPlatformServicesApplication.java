abc71eff98f1bedd58c96be34e13f240077329cb
Sebastian Contras <sebastiancontras@gmail.com>
2018-03-20 22:03:51 +0200

    First commit

---
+++ elearning-platform-services/src/main/java/com/elearningplatformservices/ElearningPlatformServicesApplication.java
@@ -0,0 +1,12 @@
+package com.elearningplatformservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElearningPlatformServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElearningPlatformServicesApplication.class, args);
+	}
+}
