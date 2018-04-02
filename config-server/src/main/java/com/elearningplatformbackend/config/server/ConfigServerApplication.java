74a8c4abd4b5fcdc54f3c05e091bbe749e448592
Bogdan <bogdangeorgelet@gmail.com>
2018-04-02 18:15:53 +0300

    renamed config server

--- config-server/src/main/java/com/elearningplatformbackend/config/server/ConfigServerApplication.java
+++ config-server/src/main/java/com/elearningplatformbackend/config/server/ConfigServerApplication.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class ElearningplatformbackendConfigServerApplication {
+public class ConfigServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ElearningplatformbackendConfigServerApplication.class, args);
+		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 }
