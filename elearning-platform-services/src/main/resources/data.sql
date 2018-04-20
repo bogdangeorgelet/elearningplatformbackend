52b3afe5d80cb9f5e5f3bd2c18247c63433fd31c
Bogdan Let <bogdangeorgelet@gmail.com>
2018-04-20 16:49:39 +0300

    fixed the relation with course - customer and course - instructor

--- elearning-platform-services/src/main/resources/data.sql
+++ elearning-platform-services/src/main/resources/data.sql
@@ -5,7 +5,10 @@ INSERT INTO INSTRUCTORS (ID, FIRSTNAME, LASTNAME, PASSWORD, EMAIL, DATE_CREATED)
 VALUES (2, 'Bogdan', 'George', 'test1234', 'bg@gmail.com', '2018-05-17');
 
 INSERT INTO CUSTOMERS (ID, FULLNAME, USERNAME, PASSWORD, EMAIL, ADDRESS, PHONE_NUMBER)
-VALUES (1, 'carolinedietmar', 'abcd1234', 'Caroline Dietmar', 'carolinedietmar@gmail.com', 'Str. Nufarului', '0720123456');
+VALUES (2, 'Caroline Dietmar', 'abcd1234', 'test', 'carolinedietmar@gmail.com', 'Str. Nufarului', '0720123456');
+
+INSERT INTO CUSTOMERS (ID, FULLNAME, USERNAME, PASSWORD, EMAIL, ADDRESS, PHONE_NUMBER)
+VALUES (1, 'Sam Patrick', 'sampatrick', 'testpassword', 'sampatrick@gmail.com', 'Str. Nufarului', '0720123456');
 
 INSERT INTO COURSES (ID, NAME, COURSE_TYPE, PRICE, INSTRUCTOR_ID)
 VALUES (1, 'IT', 'Angular 2 for beginners', '99.9', '1');
@@ -15,3 +18,6 @@ VALUES (2, 'IT', 'PHP', '10', '2');
 
 INSERT INTO COURSES_CUSTOMER (COURSES_ID, CUSTOMER_ID)
 VALUES ('1', '1');
+
+INSERT INTO COURSES_CUSTOMER (COURSES_ID, CUSTOMER_ID)
+VALUES ('2', '2');
